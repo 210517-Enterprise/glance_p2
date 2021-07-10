a9fdccdf7b36a7729584aacf27cb7a6ee11971de
jhw2167 <jhw2167@columbia.edu>
2021-07-10 03:23:29 -0400

    also added goal class

---
+++ Glance/src/main/java/com/revature/models/Goal.java
@@ -0,0 +1,20 @@
+package com.revature.models;
+
+public class Goal {
+
+	/*
+	 * Goal Class will hold simple spending and saving goals
+	 * associated with a user
+	 * 	Prospective variables
+	 * 	isSpendingGoal: boolean		//alternative is saving goal
+	 * 	userID: int		//from our db
+	 * 	accID:	int		//from our db
+	 *  startDate: localDateTime	//start of goal
+	 *  endDate: localDateTime		//end of goal
+	 *  isComplete:	boolean		//is the goal completed successfully
+	 * 
+	 */
+	
+	
+	
+}
