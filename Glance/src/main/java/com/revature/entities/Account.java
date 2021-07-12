d2e9a2cfb7c88b73e827e7bb54ef01046c834f93
jhw2167 <jhw2167@columbia.edu>
2021-07-12 17:47:36 -0400

    deleted models package, we use entities to refer to User, Account, etc now

--- Glance/src/main/java/com/revature/entities/Account.java
+++ Glance/src/main/java/com/revature/entities/Account.java
@@ -2,4 +2,18 @@ package com.revature.entities;
 
 public class Account {
 
+	/*Class used to hold account data we are interested in
+	 * and return it to the front end in JSON format
+	 * Spring bean, JPA annotated object
+	 * 	- balance:  double
+	 *  - account_id: int GENERATED
+	 *  - account_num: int
+	 *  - transactions: List<Transactions>
+	 *  - account_type: string
+	 *  
+	 *  Important Methods:
+	 *  stringAsJSON: String	//returns JSON string to interpret Acc on front end
+	 *  
+	 *  Author: Kyle Castillo
+	 */
 }
