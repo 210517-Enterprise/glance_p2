f8e16b774e4a4b238476f87efe78aeeab7edaeb5
jhw2167 <jhw2167@columbia.edu>
2021-07-14 22:29:29 -0400

    removed misc code from driver file

--- Glance/src/main/java/com/revature/GlanceApplication.java
+++ Glance/src/main/java/com/revature/GlanceApplication.java
@@ -3,11 +3,18 @@ package com.revature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.revature.entities.User;
+import com.revature.exceptions.InvalidPasswordException;
+import com.revature.exceptions.NoSuchTupleException;
+import com.revature.service.UserService;
+
 @SpringBootApplication
 public class GlanceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GlanceApplication.class, args);
+	
+
 	}
 
 }
